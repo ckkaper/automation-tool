1026716ada1d2802510ebc0ef9d95171312986fc
kyriakos <ckkaperonis@gmail.com>
2024-08-18 14:48:55 +0300

    init commit automation script

---
+++ automation.py
@@ -0,0 +1,24 @@
+import subprocess
+import requests
+import time
+
+print("Running docker compose up")
+
+subprocess.call(["docker", "compose", "up", "-d"])
+while True:
+    try: 
+        healthcheckResult = requests.get("http://localhost:3000/ping", timeout=1)
+        if healthcheckResult.ok:
+            print("Successfully recieved healthcheck result")
+            break
+    except:
+        print("Pending for healthcheck result")
+    
+    time.sleep(3)
+
+
+print("Running prisma database seed")
+
+subprocess.call(["docker", "exec", "-it", "identity-app-skeleton-svc-1", "node", "./dist/prisma/seed.js"])
+
+print("Successfully ran prisma seed")
